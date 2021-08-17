58289299c7cfa252a0598a6ac6a846ec43f6b5d2
Gaurav Singh <gauravsingh.info@gmail.com>
2021-08-17 22:26:46 +0530

    added bootstrap, created Navbar & Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='container'>
+            <footer className="mt-auto text-dark-50 text-center">
+                <p>Created By <a href='https://www.linkedin.com/in/gaurav-singh-17b9a873/' className='text-dark'>Gaurav Singh</a></p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
